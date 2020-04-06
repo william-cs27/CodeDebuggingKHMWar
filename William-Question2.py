8eab4b62201367ebbf3d22f8cfb7c6761d15871e
william-cs27 <williamcsam@gail.com>
2020-04-06 22:58:43 +0530

    new changes

--- William-Question2.py
+++ William-Question2.py
@@ -6,10 +6,10 @@ for i in range(int(input())):
         word.upper() 
         vowels = ['A','E','I','O','U']
         count=0
-        for j in range(1,len(word)) :
+        for j in range(1,len(word)) :  #length() to len() and ":" is placed to make the synatx of for loop correct
             
             if (word[j] in vowels) : 
-                count=count+1 
+                count=count+1               #incrementing in classic method
                 if (word[j] in vowels) and (word[j+1] in vowels):
                     if word[j-1] in vowels:
                         count=count-1
